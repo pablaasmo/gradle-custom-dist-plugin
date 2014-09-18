b49f75bc69e620cd1a91e7a682ee3b149aeb150a
Jan Sorensen <jas@praqma.net>
2014-09-18 15:51:05 +0200

    Fix problem with generating top-level folder in distribution zip. Support for a url or version for a base distribution.

--- src/main/groovy/net/praqma/gradle/customdistplugin/CustomGradleDistribution.groovy
+++ src/main/groovy/net/praqma/gradle/customdistplugin/CustomGradleDistribution.groovy
@@ -1,7 +1,11 @@
 package net.praqma.gradle.customdistplugin;
 
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.bundling.Zip
+import org.gradle.util.DistributionLocator
+import org.gradle.util.GradleVersion
 import org.gradle.wrapper.Download
 import org.gradle.wrapper.IDownload
 import org.gradle.wrapper.Install
@@ -10,21 +14,64 @@ import org.gradle.wrapper.WrapperConfiguration
 
 class CustomGradleDistribution extends Zip {
 
-	/** URL of the Gradle distribution to customize */
-	@Input
+	/**
+	 * URL of the Gradle distribution to customize.
+	 * <p>
+	 * Either baseDistribution or gradleVersion must be set, but not both.
+	 */
+	@Optional @Input
 	String baseDistribution
 
+
+	/** 
+	 * Version of the base Gradle distribution.
+	 * <p>
+	 * Either baseDistribution or gradleVersion must be set, but not both.
+	 */
+	@Optional @Input
+	String gradleVersion
+
+	/**
+	 * The name of the generated Gradle distribution.
+	 * <p>
+	 * It is use as the name of the top-level folder in the zip. It defaults to
+	 * the archiveName with the extension removed.
+	 */
+	@Optional @Input
+	String distributionName
+
 	CustomGradleDistribution() {
+		into {
+			distributionName ?: archiveName.lastIndexOf('.').with {
+				it == -1 ? archiveName : archiveName[0..<it]
+			}
+		}
 		from { downloadGradleDistribution() }
 	}
 
 	/** Download and install a Gradle distribution */
 	File downloadGradleDistribution() {
+		WrapperConfiguration conf = new WrapperConfiguration()
+		conf.distribution = getBaseDistributionUri()
 		PathAssembler pathAssembler = new PathAssembler(project.gradle.gradleUserHomeDir)
 		IDownload download = new Download(project.name, project.version)
 		Install install = new Install(download, pathAssembler)
-		WrapperConfiguration conf = new WrapperConfiguration()
-		conf.distribution = URI.create(baseDistribution)
 		return install.createDist(conf)
 	}
+
+	private URI getBaseDistributionUri() {
+		if (baseDistribution != null && gradleVersion != null) {
+			throw new GradleException("Both 'baseDistribution' and 'gradleVersion' is defined. Only set one of them.")
+		}
+		if (baseDistribution == null && gradleVersion == null) {
+			throw new GradleException("Either 'baseDistribution' or 'gradleVersion' must be set.")
+		}
+		if (gradleVersion != null) {
+			def locator = new DistributionLocator()
+			return locator.getDistributionFor(GradleVersion.version(gradleVersion))
+		} else {
+			URI.create(baseDistribution)
+		}
+	}
+
 }
